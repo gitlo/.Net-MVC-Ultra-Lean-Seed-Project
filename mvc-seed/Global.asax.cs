626c9707e9c51a0751ceb8f865ffa9e474af54a8
mcptech <kezman01@gmail.com>
2015-11-19 11:49:13 +0000

    first release

---
+++ mvc-seed/Global.asax.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace mvc_seed
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            RouteTable.Routes.MapRoute("default route", "{controller}/{action}/{id}",
+                new {controller = "Home", action = "Index", id = UrlParameter.Optional});
+        }
+    }
+}
